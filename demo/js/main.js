49a84f620972b7e51b2b12de8cc38cf817f63a58
myvipbackup2 <lzxvipmail@vip.qq.com>
2016-11-25 20:30:26 +0800

    完成项目

--- demo/js/main.js
+++ demo/js/main.js
@@ -4,6 +4,7 @@
     var aImg = oContainer.getElementsByTagName('img');
     var zIndex = 1;
     var collide = [];
+    var nearElem = null;
 
     for (var i = aImg.length - 1; i >= 0; i--) {
         aImg[i].style.left = aImg[i].offsetLeft + 'px';
@@ -40,11 +41,26 @@
                         collide.push(aImg[i]);
                     }
                 }
+                if (collide) {
+                    nearElem = nearest(elem);
+                } else {
+                    nearElem = null;
+                }
                 return false;
             };
 
             document.onmouseup = function () {
                 document.onmousemove = null;
+                if (nearElem) {
+                    animate(elem,nearElem.pos);
+                    animate(nearElem,elem.pos);
+                    var tmpPos = elem.pos;
+                    elem.pos = nearElem.pos;
+                    nearElem.pos = tmpPos;
+                    nearElem = null;
+                }else {
+                    animate(elem,elem.pos);
+                }
 
             };
         }
@@ -56,12 +72,27 @@
         var elemR = elem.offsetLeft + elem.offsetWidth,
             elemB = elem.offsetTop + elem.offsetHeight,
             elemT = elem.offsetTop,
-            elemL= elem.offsetLeft;
+            elemL = elem.offsetLeft;
         var targetR = target.offsetLeft + target.offsetWidth,
             targetB = target.offsetTop + target.offsetHeight,
             targetT = target.offsetTop,
             targetL = target.offsetLeft;
-        return ! (elemR < targetL || elemB<targetT || elemL > targetR || elemT >targetB);
+        return !(elemR < targetL || elemB < targetT || elemL > targetR || elemT > targetB);
+    }
+
+    function nearest(elem) {
+        var minDis = 9999;
+        var index = -1;
+        for (var i = 0; i < collide.length; i++) {
+            var x = collide[i].offsetTop - elem.offsetTop;
+            var y = collide[i].offsetLeft - elem.offsetLeft;
+            var iDis = Math.sqrt(x * x + y * y);
+            if (iDis<minDis){
+                minDis = iDis;
+                index = i;
+            }
+        }
+        return collide[index];
     }
 
 
