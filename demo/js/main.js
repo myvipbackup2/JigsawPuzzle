ea9abba06b14c2293383957cee00fd3c1f1a7a5a
myvipbackup2 <lzxvipmail@vip.qq.com>
2016-11-25 19:56:22 +0800

    完成拖拽

--- demo/js/main.js
+++ demo/js/main.js
@@ -2,13 +2,46 @@
 
     var oContainer = document.getElementById('container');
     var aImg = oContainer.getElementsByTagName('img');
+    var zIndex = 1;
+    var collide = [];
 
     for (var i = aImg.length - 1; i >= 0; i--) {
         aImg[i].style.left = aImg[i].offsetLeft + 'px';
         aImg[i].style.top = aImg[i].offsetTop + 'px';
         aImg[i].style.position = 'absolute';
-        aImg[i].style.margin = 0;
+        aImg[i].pos = {
+            left : aImg[i].offsetLeft,
+            top : aImg[i].offsetTop
+        };
+        drag(aImg[i]);
     }
+
+    function drag(elem) {
+
+        elem.onmousedown = function (e) {
+            e =e || event;
+            var iDisX = e.clientX - elem.offsetLeft;
+            var iDisY = e.clientY - elem.offsetTop;
+            elem.style.zIndex = zIndex++;
+
+            document.onmousemove = function (e) {
+                e = e || event;
+                var iLeft = e.clientX - iDisX;
+                var iTop = e.clientY -iDisY;
+                elem.style.top = iTop + 'px';
+                elem.style.left = iLeft + 'px';
+
+
+                document.onmouseup = function () {
+                    document.onmousemove = null;
+                }
+            }
+        }
+
+
+    }
+
+
 
 
 })();
