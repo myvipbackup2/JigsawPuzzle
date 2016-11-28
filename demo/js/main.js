3296cb0254a701ee06ba2d0ae660d1561b36996f
myvipbackup2 <lzxvipmail@vip.qq.com>
2016-11-28 20:53:37 +0800

    修改random

--- demo/js/main.js
+++ demo/js/main.js
@@ -98,7 +98,7 @@ window.onload = function () {
 
         function random() {
             var arr = [1,2,3,4,5,6,7,8,9];
-            arr.sort(function (a, b) {
+            arr.sort(function () {
                 return Math.random() -0.5;
             });
             for(var i=0;i<aImg.length;i++){
